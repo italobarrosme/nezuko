b357f1ddf6b4e4ae109537e0e48185dce3cacb66
italobarrosme <italobarros.me@gmail.com>
2023-02-21 08:14:14 -0300

    feat: implement test loading

--- src/useComponents/Loading/Loading.tsx
+++ src/useComponents/Loading/Loading.tsx
@@ -5,9 +5,18 @@ export const Loading = () => {
       data-testid="loading"
     >
       <div className="h-screen flex items-center justify-center space-x-2">
-        <div className="w-4 h-4 rounded-full animate-pulse bg-brand-secondary"></div>
-        <div className="w-4 h-4 rounded-full animate-pulse bg-brand-secondary"></div>
-        <div className="w-4 h-4 rounded-full animate-pulse bg-brand-secondary"></div>
+        <div
+          className="w-4 h-4 rounded-full animate-pulse bg-brand-secondary"
+          data-testid="dot"
+        ></div>
+        <div
+          className="w-4 h-4 rounded-full animate-pulse bg-brand-secondary"
+          data-testid="dot"
+        ></div>
+        <div
+          className="w-4 h-4 rounded-full animate-pulse bg-brand-secondary"
+          data-testid="dot"
+        ></div>
       </div>
     </div>
   )
