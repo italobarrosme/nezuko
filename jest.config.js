094f036d0bb4be99be32ce31856844a397c33014
italobarrosme <italobarros.me@gmail.com>
2023-08-15 16:12:49 -0300

    feat: add vitest

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
-  collectCoverage: false,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-}
